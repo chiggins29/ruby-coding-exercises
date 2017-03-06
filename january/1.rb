e38ffa8e6d1bbc2b560a76df0053d192014a865f
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-06 09:50:22 -0700

    completed january 1st ruby exercise

--- january/1.rb
+++ january/1.rb
@@ -1,8 +1,11 @@
 require 'rspec'
 
 def title_creator title
-	
+	heading = %w{h1 #{title} h1}
+	heading[1] = title
+	'<%s>%s</%s>' % heading
 end
+	
 
 describe 'HTML h1 converter' do
   it 'outputs an h1 tag given a title' do
